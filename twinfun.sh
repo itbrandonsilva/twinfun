f2870468485a25f6ade526f00297fd297b23d63d
Brandon Silva <itbrandonsilva@gmail.com>
2013-07-15 22:28:39 -0400

    Added some git settings.

--- twinfun.sh
+++ twinfun.sh
@@ -8,3 +8,9 @@ a "set tabstop=4";
 a "set shiftwidth=4";
 a "set expandtab";
 a "set hlsearch";
+
+if type git &> /dev/null; then
+    git config --global user.name   "Brandon Silva";
+    git config --global user.email  "itbrandonsilva@gmail.com";
+    git config --global core.editor vim;
+fi
