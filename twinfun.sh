e50b322d9db5740ee8264a67127a6396585ccc1e
Brandon Silva <itbrandonsilva@gmail.com>
2013-08-30 06:02:57 -0400

    Added config for tmux. Added ef function.

--- twinfun.sh
+++ twinfun.sh
@@ -1,18 +1,39 @@
-function a () {
-    echo -e "$1" >> "$F";
+# ensure file
+function ef () {
+    if [ ! -f "$F" ]
+    then
+        touch "$F"; 
+        echo "created file \" $F \"";
+    fi
 }
 
-cd ~/;
+# append
+function a () {
+    ef;
+    if ! grep -xq "$1" "$F"
+    then
+        echo -e "$1" >> "$F";
+    fi
+}
 
-F=.vimrc;
+echo -e ""
+
+F=~/.vimrc;
 a "set nowrap";
 a "set tabstop=4";
 a "set shiftwidth=4";
 a "set expandtab";
 a "set hlsearch";
 
+F=~/.tmux.conf;
+a "set -g base-index 1";
+a "set -g pane-base-index 1";
+
 if type git &> /dev/null; then
     git config --global user.name   "Brandon Silva";
     git config --global user.email  "itbrandonsilva@gmail.com";
     git config --global core.editor vim;
 fi
+
+echo -e "done.";
+echo -e "";
