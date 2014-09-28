d3c529caa1de876171c6e6235351d977113064c5
Brandon Silva <itbrandonsilva@gmail.com>
2014-09-28 11:24:27 -0700

    More cleanup, some additions.

--- twinfun.sh
+++ twinfun.sh
@@ -1,15 +1,5 @@
 #!/bin/bash
 
-if [ "$1" = "init" ]; then
-    # Ubuntu 14
-
-    # Latest stable git
-    add-apt-repository http://ppa.launchpad.net/git-core/ppa/ubuntu;
-    apt-get update;
-    apt-get -y --force-yes upgrade;
-    apt-get -y --force-yes install git vim tmux build-essential iftop;
-fi
-
 # ensure file exists
 function ef () {
     if [ ! -f "$F" ]
@@ -28,6 +18,23 @@ function a () {
         echo -e "$1" >> "$F";
     fi
 }
+
+if [ "$1" = "init" ]; then
+    # Ubuntu 14
+
+    # Latest stable git
+    add-apt-repository http://ppa.launchpad.net/git-core/ppa/ubuntu;
+
+    # Latest stable google-chrome
+    wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -;
+    sh -c 'echo "deb http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
+
+    apt-get update;
+    apt-get -y --force-yes upgrade;
+    apt-get -y --force-yes install git vim tmux build-essential iftop google-chrome-stable;
+    F=~/.bashrc;
+    a "alias chrome=google-chrome-stable";
+fi
 
 echo -e ""
 
@@ -56,5 +63,10 @@ F=~/.sqliterc;
 a ".headers on";
 a ".mode column";
 
+echo -e ""
+
+if [ "$1" = "init" ]; then
+    echo -e "Changes were potentially made to your .bashrc, be sure to source it.";
+fi
 echo -e "done.";
 echo -e "";
