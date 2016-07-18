1fb698c206cbbc10705a2a2057f982a9540c477b
Brandon Silva <itbrandonsilva@gmail.com>
2016-07-18 04:47:09 -0400

    Force yes.

--- bin/setup-chrome.sh
+++ bin/setup-chrome.sh
@@ -2,4 +2,4 @@
 
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo dpkg -i --force-depends google-chrome-stable_current_amd64.deb
-sudo apt-get install -f
+sudo apt-get install -y --force-yes -f
